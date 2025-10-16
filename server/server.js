bd05d6a7e997ba381cb1e09de3e6f2490deb8868
0101Keke <kekeletsomaleka04@gmail.com>
2025-10-16 13:10:21 +0200

    uninstalled and reinstalled npm package, also fixed somethings.

--- server/server.js
+++ server/server.js
@@ -13,7 +13,8 @@ app.use(cors());
 app.use(express.json({ limit: '10mb' }));
 
 // Connect to DB
-connectDB(process.env.MONGO_URI);
+connectDB(process.env.MONGODB_URI);
+
 
 // Routes
 router.post('/tutors/:id/topics/:topicId/response', async (req, res) => {
