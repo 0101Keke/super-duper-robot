b8ae5717225954b4c0b51cf92e17920533aef285
MonoxTI <itumonokoane84@gmail.com>
2025-10-16 19:04:11 +0200

    Trying to run the frontend.

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
@@ -6,11 +5,3 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
 })
-=======
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-
-export default defineConfig({
-    plugins: [react()],
-});
->>>>>>> f63ec98c6e0e741fb5fdbe84aa54d3c7c6ffc222
